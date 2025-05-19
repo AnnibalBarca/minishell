db7c7fb1a8b9e667fef38e7ad1227aad2ec5c677
Alexandre Meekel-Konjikovic <almeekel@z4r2p5.42lyon.fr>
2025-05-19 20:08:03 +0200

    expander updated in order to get the IFS variable in the environment and send it dupped if found, NULL if not found

--- src/parsing/expander_utils.c
+++ src/parsing/expander_utils.c
@@ -6,44 +6,71 @@
 /*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 20:15:31 by almeekel          #+#    #+#             */
-/*   Updated: 2025/05/11 20:18:07 by almeekel         ###   ########.fr       */
+/*   Updated: 2025/05/19 20:05:28 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/parsing.h"
 
-// Utils pour l'expaneur
-
-// Retrouve la valeur d'une variable d'envrionnement
-//l'appel doit clear la chaine retournee si c'est un dup !!!
-// Pour $? ca retourne un char* representant g_exit_status.
 char	*get_env_var_value(const char *var_name, char **envp,
 		int last_exit_status)
 {
 	int		i;
 	size_t	name_len;
-	char	*value;
 
+	if (!var_name)
+		return (NULL);
 	if (ft_strcmp(var_name, "?") == 0)
 		return (ft_itoa(last_exit_status));
 	if (!envp)
-		return (ft_strdup("")); // Pas d'envp, retourne char * vide
+		return (NULL);
 	name_len = ft_strlen(var_name);
+	if (name_len == 0)
+		return (NULL);
 	i = 0;
 	while (envp[i])
 	{
 		if (ft_strncmp(envp[i], var_name, name_len) == 0
 			&& envp[i][name_len] == '=')
 		{
-			value = ft_strdup(envp[i] + name_len + 1);
-			return (value);
+			return (ft_strdup(envp[i] + name_len + 1));
 		}
 		i++;
 	}
-	return (ft_strdup("")); // Variable nn trouvee, retourne char * vide
+	return (NULL);
 }
 
 int	is_valid_var_char(char c)
 {
 	return (ft_isalnum(c) || c == '_');
+}
+
+int	handle_glob_matches(char **glob_matches, t_token **head, char **fields)
+{
+	int j;
+
+	if (!glob_matches)
+	{
+		free_char_array(fields);
+		free_token_list(*head);
+		return (0);
+	}
+	j = 0;
+	while (glob_matches[j])
+	{
+		if (!create_and_append_token(head, glob_matches[j], T_WORD, Q_NONE,
+				NULL))
+		{
+			while (glob_matches[j])
+				free(glob_matches[j++]);
+			free(glob_matches);
+			free_char_array(fields);
+			free_token_list(*head);
+			return (0);
+		}
+		glob_matches[j] = NULL;
+		j++;
+	}
+	free(glob_matches);
+	return (1);
 }
