759e162ef3d72aaf6832f46bcf16b2f8683cc13a
Alexandre Meekel-Konjikovic <almeekel@z4r2p5.42lyon.fr>
2025-05-19 18:08:06 +0200

    exit and cd model

---
+++ src/builtins/exit.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 16:25:04 by almeekel          #+#    #+#             */
+/*   Updated: 2025/05/19 16:27:40 by almeekel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/parsing.h"
+
+static int	is_numeric(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	builtin_exit(char **args, t_shell *shell)
+{
+	unsigned char status;
+
+	ft_putendl_fd("exit", STDERR_FILENO);
+	if (!args[1])
+	{
+		shell->should_exit = 1;
+		return (g_exit_status);
+	}
+	if (!is_numeric(args[1]))
+	{
+		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd(args[1], STDERR_FILENO);
+		ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+		shell->should_exit = 1;
+		return (255);
+	}
+	if (args[1] && args[2])
+	{
+		ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
+		return (1);
+	}
+	status = (unsigned char)ft_atoi(args[1]);
+	shell->should_exit = 1;
+	return (status);
+}
