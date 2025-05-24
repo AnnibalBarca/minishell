e719814cd1ac4171b520fa133125047bef71db34
Alexandre Meekel-Konjikovic <almeekel@z4r6p4.42lyon.fr>
2025-05-24 12:09:57 +0200

    exec modifying for switching to sacha (Alex)

--- includes/exec.h
+++ includes/exec.h
@@ -6,7 +6,7 @@
 /*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 15:12:21 by nagaudey          #+#    #+#             */
-/*   Updated: 2025/05/23 18:37:34 by almeekel         ###   ########.fr       */
+/*   Updated: 2025/05/24 12:09:31 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,5 @@ void	execute_bonus(t_exec *exec, char *argv, char **envp);
 int		pipex(t_exec *exec, int ac, char **av, char **envp);
 void	exec_one(t_exec *exec, char *argv, char **envp);
 void	random_filename(t_exec *exec);
-t_pipex_input	prepare_pipeline_for_pipex(t_token *expanded_tokens);
-
 
 #endif
