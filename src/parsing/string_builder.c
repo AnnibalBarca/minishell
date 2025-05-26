68e05f169178993dc9a3df174525b111d99efc60
Alexandre Meekel-Konjikovic <almeekel@z4r5p5.42lyon.fr>
2025-05-26 15:30:44 +0200

    safer sb with checker funct

--- src/parsing/string_builder.c
+++ src/parsing/string_builder.c
@@ -6,7 +6,7 @@
 /*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 21:59:05 by almeekel          #+#    #+#             */
-/*   Updated: 2025/05/24 15:28:34 by almeekel         ###   ########.fr       */
+/*   Updated: 2025/05/26 15:30:02 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,46 +21,59 @@ void	sb_init(t_str_builder *sb)
 	sb->capacity = 0;
 }
 
-int	sb_append_char(t_str_builder *sb, char c)
+static int	sb_ensure_capacity(t_str_builder *sb, size_t needed)
 {
 	char	*new_str;
-	char	char_to_append[2];
+	size_t	new_cap;
 
-	if (!sb)
-		return (0);
-	char_to_append[0] = c;
-	char_to_append[1] = '\0';
-	if (!sb->str)
-		new_str = ft_strdup(char_to_append);
-	else
-	{
-		new_str = ft_strjoin(sb->str, char_to_append);
-		free(sb->str);
-	}
+	if (needed <= sb->capacity)
+		return (1);
+	new_cap = sb->capacity;
+	if (new_cap == 0)
+		new_cap = 16;
+	while (new_cap < needed)
+		new_cap *= 2;
+	new_str = ft_calloc(new_cap + 1, sizeof(char));
 	if (!new_str)
 		return (0);
+	if (sb->str)
+		ft_strlcpy(new_str, sb->str, sb->len + 1);
+	free(sb->str);
 	sb->str = new_str;
-	sb->len = ft_strlen(new_str);
+	sb->capacity = new_cap;
+	return (1);
+}
+
+int	sb_append_char(t_str_builder *sb, char c)
+{
+	if (!sb)
+		return (0);
+	if (!sb_ensure_capacity(sb, sb->len + 1))
+		return (0);
+	sb->str[sb->len] = c;
+	sb->len++;
+	sb->str[sb->len] = '\0';
 	return (1);
 }
 
 int	sb_append_str(t_str_builder *sb, const char *s)
 {
-	char	*new_str;
+	size_t	s_len;
+	size_t	i;
 
 	if (!sb || !s)
 		return (0);
-	if (!sb->str)
-		new_str = ft_strdup(s);
-	else
-	{
-		new_str = ft_strjoin(sb->str, s);
-		free(sb->str);
-	}
-	if (!new_str)
+	s_len = ft_strlen(s);
+	if (!sb_ensure_capacity(sb, sb->len + s_len))
 		return (0);
-	sb->str = new_str;
-	sb->len = ft_strlen(new_str);
+	i = 0;
+	while (i < s_len)
+	{
+		sb->str[sb->len + i] = s[i];
+		i++;
+	}
+	sb->len += s_len;
+	sb->str[sb->len] = '\0';
 	return (1);
 }
 
@@ -84,12 +97,7 @@ char	*sb_to_string_and_free(t_str_builder *sb)
 	char	*result;
 
 	if (!sb || !sb->str)
-	{
-		result = ft_strdup("");
-		if (!result)
-			return (NULL);
-		return (result);
-	}
+		return (ft_strdup(""));
 	result = ft_strdup(sb->str);
 	sb_free(sb);
 	return (result);
