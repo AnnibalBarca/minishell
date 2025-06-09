d54337554daeba7545ab2461820832391132dc5a
Alexandre Meekel-Konjikovic <almeekel@z4r3p4.42lyon.fr>
2025-06-09 20:45:22 +0200

    utils using builtin utils soon to be passed to util folder

--- src/parsing/expander_utils.c
+++ src/parsing/expander_utils.c
@@ -6,37 +6,25 @@
 /*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 20:15:31 by almeekel          #+#    #+#             */
-/*   Updated: 2025/05/29 16:24:52 by almeekel         ###   ########.fr       */
+/*   Updated: 2025/06/08 20:48:49 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../includes/builtins.h"
 #include "../../includes/parsing.h"
 
 char	*get_env_var_value(const char *var_name, char **envp,
 		int last_exit_status)
 {
-	int		i;
-	size_t	name_len;
+	char	*value;
 
 	if (!var_name)
 		return (NULL);
 	if (ft_strcmp(var_name, "?") == 0)
 		return (ft_itoa(last_exit_status));
-	if (!envp)
-		return (NULL);
-	name_len = ft_strlen(var_name);
-	if (name_len == 0)
-		return (NULL);
-	i = 0;
-	while (envp[i])
-	{
-		if (ft_strncmp(envp[i], var_name, name_len) == 0
-			&& envp[i][name_len] == '=')
-		{
-			return (ft_strdup(envp[i] + name_len + 1));
-		}
-		i++;
-	}
+	value = find_env_var(envp, var_name);
+	if (value)
+		return (ft_strdup(value));
 	return (NULL);
 }
 
@@ -44,4 +32,3 @@ int	is_valid_var_char(char c)
 {
 	return (ft_isalnum(c) || c == '_');
 }
-
