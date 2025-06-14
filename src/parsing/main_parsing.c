f08d1d08e1e840d018365abb605d98e14a85f2d4
Alexandre Meekel-Konjikovic <almeekel@z4r4p2.42lyon.fr>
2025-06-14 20:37:21 +0200

    parsing perfect, waiting exec for further testing and implementing fully the builtins

--- src/parsing/main_parsing.c
+++ src/parsing/main_parsing.c
@@ -6,7 +6,7 @@
 /*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 15:31:48 by almeekel          #+#    #+#             */
-/*   Updated: 2025/06/12 18:49:10 by almeekel         ###   ########.fr       */
+/*   Updated: 2025/06/14 20:31:08 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ t_token	*parse_complete_input(char **accumulated_input, char **envp,
 	char			*line;
 	t_token			*tokens;
 	t_syntax_result	syntax_result;
+	t_token			*expanded_tokens;
 
 	line = *accumulated_input;
 	if (check_signal_interruption())
@@ -45,21 +46,24 @@ t_token	*parse_complete_input(char **accumulated_input, char **envp,
 				NULL));
 	if (has_unclosed_quotes(line))
 		return (cleanup_parsing_and_return_null(&line, &accumulated_input, NULL,
-				"minishell: syntax error: unclosed quotes\n"));
+				"minishell: syntax error: unclosed quotes\n"));	
 	tokens = lexer(line);
+	// print_token_list(tokens, "AFTER LEXING");
 	if (!tokens)
-		return (cleanup_parsing_and_return_null(&line, &accumulated_input, NULL,
+		return (cleanup_parsing_and_return_null(&line, &accumulated_input, &tokens,
 				NULL));
 	syntax_result = analyze_syntax(tokens);
 	if (syntax_result.status == PARSE_OK)
 	{
 		*accumulated_input = line;
-		return (process_complete_syntax(syntax_result, envp, exit_status));
+		expanded_tokens = process_complete_syntax(syntax_result, envp, exit_status);
+		// print_token_list(expanded_tokens, "AFTER EXPANSION");
+		return (expanded_tokens);
 	}
 	else
 	{
 		handle_syntax_error(&syntax_result);
-		return (cleanup_parsing_and_return_null(&line, &accumulated_input, NULL,
+		return (cleanup_parsing_and_return_null(&line, &accumulated_input, &tokens,
 				NULL));
 	}
 }
