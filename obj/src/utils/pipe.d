d7f39591a8c899fbf760afb302c86097ec4407c6
nagaudey <ngaudey@gmail.com>
2025-06-16 19:48:47 +0200

    exec

--- obj/src/utils/pipe.d
+++
@@ -1,9 +0,0 @@
-obj/src/utils/pipe.o: src/utils/pipe.c src/utils/../../includes/utils.h \
-  src/utils/../../includes/struct.h \
-  src/utils/../../includes/../libft/all.h
-
-src/utils/../../includes/utils.h:
-
-src/utils/../../includes/struct.h:
-
-src/utils/../../includes/../libft/all.h:
