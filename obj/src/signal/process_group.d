943e0a94a2f1c459c10690a06173ad01a47795a5
nagaudey <ngaudey@gmail.com>
2025-07-17 18:54:19 +0200

    signal and norm

--- obj/src/signal/process_group.d
+++
@@ -1,5 +0,0 @@
-obj/src/signal/process_group.o: src/signal/process_group.c \
- includes/signals.h includes/struct.h includes/../libft/all.h
-includes/signals.h:
-includes/struct.h:
-includes/../libft/all.h:
--- obj/src/signal/signal_setup.d
+++
@@ -1,8 +0,0 @@
-obj/src/signal/signal_setup.o: src/signal/signal_setup.c \
-  includes/signals.h includes/struct.h includes/../libft/all.h
-
-includes/signals.h:
-
-includes/struct.h:
-
-includes/../libft/all.h:
