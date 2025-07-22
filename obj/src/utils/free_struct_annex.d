2302b783581ab8e4cf85dcc260ebfa7b32aeb528
nagaudey <ngaudey@gmail.com>
2025-07-22 12:19:01 +0200

    save

--- obj/src/utils/free_struct_annex.d
+++
@@ -1,9 +0,0 @@
-obj/src/utils/free_struct_annex.o: src/utils/free_struct_annex.c \
-  src/utils/../../includes/utils.h includes/struct.h \
-  includes/../libft/all.h
-
-src/utils/../../includes/utils.h:
-
-includes/struct.h:
-
-includes/../libft/all.h:
--- obj/src/utils/shelvl_pwd_helpers.d
+++
@@ -1,9 +0,0 @@
-obj/src/utils/shelvl_pwd_helpers.o: src/utils/shelvl_pwd_helpers.c \
-  src/utils/../../includes/utils.h includes/struct.h \
-  includes/../libft/all.h
-
-src/utils/../../includes/utils.h:
-
-includes/struct.h:
-
-includes/../libft/all.h:
